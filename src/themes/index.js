27da115d18d19b079f1d3445b0863afccd114126
antoine92190 <dupontantoine92@gmail.com>
2020-03-22 09:11:39 +1100

    (theme) improve default box shadow

--- src/themes/index.js
+++ src/themes/index.js
@@ -14,7 +14,7 @@ export const defaultThemeStyles = {
 			border: 'none',
 			borderRadius: '4px',
 			boxShadow:
-				'0px 3px 1px -2px rgba(0, 0, 0, 0.2), 0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0, 0, 0, 0.12)'
+				'0px 1px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0, 0, 0, 0.12)'
 		},
 
 		header: {
@@ -124,7 +124,7 @@ export const defaultThemeStyles = {
 			border: 'none',
 			borderRadius: '4px',
 			boxShadow:
-				'0px 3px 1px -2px rgba(0, 0, 0, 0.2), 0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0, 0, 0, 0.12)'
+				'0px 1px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0, 0, 0, 0.12)'
 		},
 
 		header: {
