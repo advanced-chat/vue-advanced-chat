8fb13e199c181ad1cb91e70aaefd06bb7ef1d248
antoine92190 <dupontantoine92@gmail.com>
2020-05-21 13:50:37 +1100

    (test) text formatting

--- src/utils/formatString.js
+++ src/utils/formatString.js
@@ -2,9 +2,20 @@ const linkify = require('linkifyjs')
 import linkifyHtml from 'linkifyjs/html'
 
 export default (text, doLinkify = false) => {
+	text = testString(text, '*', 'b')
+	text = testString(text, '_', 'i')
+	text = testString(text, '~', 'del')
+	text = testString(text, '|', 'ins')
+
+	// text = linkifyHtml(text, {
+	// 	defaultProtocol: 'https'
+	// })
+
+	// return text
+
 	const strings = text.split(' ')
 
-	const formattedStrings = strings.map((string, i) => {
+	let formattedStrings = strings.map((string, i) => {
 		const links = linkify.find(string)
 
 		let result = ''
@@ -17,30 +28,9 @@ export default (text, doLinkify = false) => {
 				})
 			}
 		} else {
-			if (string.length > 2) {
-				const formats = []
-
-				string = testString(string, '*', 'b', formats)
-				string = testString(string, '_', 'i', formats)
-
-				string = testString(string, '*', 'b', formats)
-				string = testString(string, '~', 'del', formats)
-
-				string = testString(string, '*', 'b', formats)
-				string = testString(string, '_', 'i', formats)
-				string = testString(string, '|', 'ins', formats)
-
-				string = testString(string, '*', 'b', formats)
-				string = testString(string, '_', 'i', formats)
-				string = testString(string, '~', 'del', formats)
-
-				formats.map(format => {
-					string = `<${format}>${string}</${format}>`
-				})
-
-				result = { bind: formats.length, content: string }
-			} else {
-				result = { content: string }
+			result = {
+				content: string,
+				bind: ['<b>', '<i>', '<del>', '<ins>'].some(el => string.includes(el))
 			}
 		}
 
@@ -50,14 +40,52 @@ export default (text, doLinkify = false) => {
 		return result
 	})
 
+	// formattedStrings = formattedStrings.map((string, i) => {
+	// 	if (
+	// 		!string.bind &&
+	// 		formattedStrings[i + 1] &&
+	// 		!formattedStrings[i + 1].bind
+	// 	) {
+	// 		string.content = string.content + ' ' + formattedStrings[i + 1].content
+	// 		formattedStrings.splice(i + 1, 1)
+	// 	}
+
+	// 	return string
+	// })
+
 	return formattedStrings
 }
 
-function testString(string, sign, type, formats) {
-	if (string.slice(0, 1) === sign && string.slice(-1) === sign) {
-		string = string.slice(1, -1)
-		formats.push(type)
-	}
+function testString(string, sign, type) {
+	const indexes = getIndexes(string, sign)
+
+	indexes.map((index, i) => {
+		const even = i % 2 === 0
+		const tag = even ? `<${type}>` : `</${type}>`
+
+		let newIndex = index === 0 ? index : string.indexOf(sign)
+
+		string = setCharAt(string, newIndex, tag)
+	})
 
 	return string
+}
+
+function setCharAt(str, index, chr) {
+	if (index > str.length - 1) return str
+	return str.substr(0, index) + chr + str.substr(index + 1)
+}
+
+function getIndexes(string, sign) {
+	const indexes = []
+
+	for (
+		let pos = string.indexOf(sign);
+		pos !== -1;
+		pos = string.indexOf(sign, pos + 1)
+	) {
+		indexes.push(pos)
+	}
+
+	return indexes
 }
