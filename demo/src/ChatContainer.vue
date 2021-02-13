f817e7708e79d7e3b14cf8c0f019d251dbef7d40
antoine92190 <dupontantoine92@gmail.com>
2021-02-13 10:46:01 +0100

    (demo) add typings example

--- demo/src/ChatContainer.vue
+++ demo/src/ChatContainer.vue
@@ -78,7 +78,7 @@ import {
 } from '@/firestore'
 import { parseTimestamp, isSameDay } from '@/utils/dates'
 import ChatWindow from './../../src/ChatWindow'
-
+// import ChatWindow, { Rooms } from 'vue-advanced-chat'
 // import ChatWindow from 'vue-advanced-chat'
 // import 'vue-advanced-chat/dist/vue-advanced-chat.css'
 // import ChatWindow from './../../dist/vue-advanced-chat.umd.min.js'
