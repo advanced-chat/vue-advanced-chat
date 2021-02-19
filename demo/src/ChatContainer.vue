6efad1dd0478cc7a7a01aef758f2647168a5eba0
antoine92190 <dupontantoine92@gmail.com>
2021-02-19 14:13:35 +0100

    (demo) remove css file import

--- demo/src/ChatContainer.vue
+++ demo/src/ChatContainer.vue
@@ -78,12 +78,8 @@ import {
 } from '@/firestore'
 import { parseTimestamp, isSameDay } from '@/utils/dates'
 import ChatWindow from './../../src/ChatWindow'
-
-// import ChatWindow from './../../dist/vue-advanced-chat.umd.min.js'
-// import './../../dist/vue-advanced-chat.css'
-
 // import ChatWindow from 'vue-advanced-chat'
-// import 'vue-advanced-chat/dist/vue-advanced-chat.css'
+// import ChatWindow from './../../dist/vue-advanced-chat.umd.min.js'
 
 export default {
 	components: {
