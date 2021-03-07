df33e2b894d628d006df1c92d5c4efe4c3b1d6a0
antoine92190 <dupontantoine92@gmail.com>
2021-03-07 11:47:42 +0100

    (fix) media type check

--- src/utils/media-file.js
+++ src/utils/media-file.js
@@ -1,9 +1,8 @@
 import { IMAGE_TYPES, VIDEO_TYPES, AUDIO_TYPES } from './constants'
 
 function checkMediaType(types, file) {
-	if (!file) return
-	const { type } = file
-	return types.some(t => type.toLowerCase().includes(t))
+	if (!file || !file.type) return
+	return types.some(t => file.type.toLowerCase().includes(t))
 }
 
 export function isImageFile(file) {
