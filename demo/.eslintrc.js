562bd5255eaca10d52a2451240c5e4f769496e9f
asitha211 <developer1@koffeemakerz.com>
2021-09-03 14:21:19 +0530

    updated  customized chat

--- demo/.eslintrc.js
+++ demo/.eslintrc.js
@@ -1,16 +1,39 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-	extends: ['plugin:vue/essential', 'eslint:recommended'],
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-empty-pattern': 'off',
-		'no-unused-vars': 'warn'
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
-  }
+	root: true,
+	env: {
+		node: true
+	},
+	extends: ['standard', 'plugin:vue/recommended'],
+	rules: {
+		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		'vue/no-unused-properties': [
+			'error',
+			{
+				groups: ['props', 'data', 'computed', 'methods', 'setup'],
+				deepData: true
+			}
+		],
+		'no-empty-pattern': 'off',
+		'no-unused-vars': 'warn',
+		'valid-typeof': 'off',
+		indent: 'off',
+		'no-tabs': 'off',
+		'vue/html-indent': 'off',
+		'space-before-function-paren': 'off',
+		'vue/max-attributes-per-line': 'off',
+		'vue/html-self-closing': [
+			'error',
+			{
+				html: {
+					void: 'always',
+					normal: 'always',
+					component: 'always'
+				}
+			}
+		]
+	},
+	parserOptions: {
+		parser: 'babel-eslint'
+	}
 }
