facbd6c29f221c944375cc3f7b66efc026c6b3f6
antoine92190 <dupontantoine92@gmail.com>
2021-11-15 22:36:44 +0100

    (chore) remove vue-compat

--- demo/vue.config.js
+++ demo/vue.config.js
@@ -4,23 +4,5 @@ module.exports = {
 		process.env.NODE_ENV === 'production' ? '/vue-advanced-chat/' : '/',
 	devServer: {
 		open: true
-	},
-
-	chainWebpack: config => {
-		config.resolve.alias.set('vue', '@vue/compat')
-
-		config.module
-			.rule('vue')
-			.use('vue-loader')
-			.tap(options => {
-				return {
-					...options,
-					compilerOptions: {
-						compatConfig: {
-							MODE: 3
-						}
-					}
-				}
-			})
 	}
 }
