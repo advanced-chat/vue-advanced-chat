6d2c6d2e1b2e02a498f9dd59fa00c40c29fcc7c2
antoine92190 <dupontantoine92@gmail.com>
2021-11-15 22:55:22 +0100

    (fix) vue duplicate import

--- demo/vue.config.js
+++ demo/vue.config.js
@@ -1,8 +1,14 @@
+const path = require('path')
+
 module.exports = {
 	lintOnSave: false,
 	publicPath:
 		process.env.NODE_ENV === 'production' ? '/vue-advanced-chat/' : '/',
 	devServer: {
 		open: true
+	},
+	chainWebpack: config => {
+		config.resolve.symlinks(false)
+		config.resolve.alias.set('vue', path.resolve('./node_modules/vue'))
 	}
 }
