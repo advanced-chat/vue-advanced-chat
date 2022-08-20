81bb98110912182b37ac3a7ecc469d5a245b0625
antoine92190 <dupontantoine92@gmail.com>
2022-08-20 09:25:58 +0200

    (chore) check if element already registered

--- src/lib/index.js
+++ src/lib/index.js
@@ -3,6 +3,10 @@ import ChatWindow from './ChatWindow'
 
 export const VueAdvancedChat = defineCustomElement(ChatWindow)
 
+const PACKAGE_NAME = 'vue-advanced-chat'
+
 export function register() {
-	customElements.define('vue-advanced-chat', VueAdvancedChat)
+	if (!customElements.get(PACKAGE_NAME)) {
+		customElements.define(PACKAGE_NAME, VueAdvancedChat)
+	}
 }
