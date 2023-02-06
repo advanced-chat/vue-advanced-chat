b0c37bb3e16294b81efb6b7597d2084e99537669
Nils Bergmann <nilsbergmann@noim.io>
2023-02-06 11:24:06 +0100

    fix: audio recording and make local demo work

--- demo/src/ChatContainer.vue
+++ demo/src/ChatContainer.vue
@@ -81,7 +81,7 @@ import * as storageService from '@/database/storage'
 import { parseTimestamp, formatTimestamp } from '@/utils/dates'
 import logoAvatar from '@/assets/logo.png'
 
-import { register } from 'vue-advanced-chat'
+import { register } from '@artesa/vue-advanced-chat'
 // import { register } from './../../dist/vue-advanced-chat.es.js'
 // import { register } from './../../src/lib/index.js'
 register()
