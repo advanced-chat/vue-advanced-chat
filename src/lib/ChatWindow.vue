5dd6379eb3433eef32016f0dc80b2c6244a5a5b8
Junior Vitor Ramisch <junior.ramisch@gmail.com>
2023-10-03 15:41:50 -0300

    fix(mobile): File explorer not opening (issue #13, pr #14)

--- src/lib/ChatWindow.vue
+++ src/lib/ChatWindow.vue
@@ -198,7 +198,7 @@ export default {
 		roomMessage: { type: String, default: '' },
 		scrollDistance: { type: Number, default: 60 },
 		acceptedFiles: { type: String, default: '*' },
-		captureFiles: { type: String, default: '' },
+		captureFiles: { type: String, default: undefined },
 		templatesText: { type: [Array, String], default: () => [] },
 		mediaPreviewEnabled: { type: [Boolean, String], default: true },
 		usernameOptions: {
