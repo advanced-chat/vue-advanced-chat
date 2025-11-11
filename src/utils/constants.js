d51e0254a4dd0f8e90530075c2bb65d31a8c5d82
Aaron Nwabuoku <aaron@chatkitty.com>
2025-11-11 18:09:37 -0500

    Reinitialize project

--- src/utils/constants.js
+++
@@ -1,3 +0,0 @@
-export const IMAGE_TYPES = ['png', 'jpg', 'jpeg', 'webp', 'svg', 'gif']
-export const VIDEO_TYPES = ['mp4', 'video/ogg', 'webm', 'quicktime']
-export const AUDIO_TYPES = ['mp3', 'audio/ogg', 'wav', 'mpeg']
